ad45957f968f7c0e4f9a4a355b6c48d1ed9705fe
Adam <adamyork@users.noreply.github.com>
2026-07-20 13:46:21 -0400

    feature: add android engine

--- src/androidMain/kotlin/com/github/adamyork/sparrow/android/AppConfig.kt
+++ src/androidMain/kotlin/com/github/adamyork/sparrow/android/AppConfig.kt
@@ -1,10 +1,11 @@
 package com.github.adamyork.sparrow.android
 
 import com.github.adamyork.sparrow.android.common.CommonConfig
+import com.github.adamyork.sparrow.android.engine.EngineConfig
 import com.github.adamyork.sparrow.android.service.ServiceConfig
 import com.github.adamyork.sparrow.platform.AppScope
 import me.tatarka.inject.annotations.Component
 
 @AppScope
 @Component
-abstract class AppConfig : CommonConfig, ServiceConfig
+abstract class AppConfig : CommonConfig, ServiceConfig, EngineConfig
---
+++ src/androidMain/kotlin/com/github/adamyork/sparrow/android/engine/EngineConfig.kt
@@ -0,0 +1,38 @@
+package com.github.adamyork.sparrow.android.engine
+
+import com.github.adamyork.sparrow.android.engine.data.AndroidCollision
+import com.github.adamyork.sparrow.android.engine.v1.AndroidEngine
+import com.github.adamyork.sparrow.platform.AppScope
+import com.github.adamyork.sparrow.platform.common.data.enemy.MapElementFactory
+import com.github.adamyork.sparrow.platform.engine.Collision
+import com.github.adamyork.sparrow.platform.engine.Engine
+import com.github.adamyork.sparrow.platform.engine.Particles
+import com.github.adamyork.sparrow.platform.engine.Physics
+import com.github.adamyork.sparrow.platform.engine.v1.DefaultParticles
+import com.github.adamyork.sparrow.platform.engine.v1.DefaultPhysics
+import com.github.adamyork.sparrow.wasm.common.data.enemy.DefaultMapElementFactory
+import me.tatarka.inject.annotations.Provides
+
+interface EngineConfig {
+
+    @AppScope
+    @Provides
+    fun provideEngine(impl: AndroidEngine): Engine = impl
+
+    @AppScope
+    @Provides
+    fun provideCollision(impl: AndroidCollision): Collision = impl
+
+    @AppScope
+    @Provides
+    fun providePhysics(impl: DefaultPhysics): Physics = impl
+
+    @AppScope
+    @Provides
+    fun provideParticles(impl: DefaultParticles): Particles = impl
+
+    @AppScope
+    @Provides
+    fun provideMapElementFactory(impl: DefaultMapElementFactory): MapElementFactory = impl
+}
+
